34eacbf9ee9eae5714ca0414d067fa7b52a9bda1
Sambidh <raisambidh@gmail.com>
2024-07-07 13:57:44 +0545

    few

--- main5.py
+++ main5.py
@@ -17,19 +17,18 @@ def find_and_merge_pdfs(directories, roll_number, output_file):
                             page = reader.pages[page_num]
                             text = page.extract_text()
                             if roll_number in text:
-                                # Split text by lines and find the line with the roll number
+                                
                                 lines = text.split('\n')
                                 for line in lines:
                                     if roll_number in line:
-                                        # Check for any valid grade in the line and ensure no invalid terms are present
+                                        
                                         if any(grade in line for grade in valid_grades) and not any(term in line for term in invalid_terms):
                                             merger.append(fileobj=pdf_file, pages=(page_num, page_num + 1))
-                                            break  # Stop after finding the first relevant page
+                                            break  
                                 else:
                                     continue
                                 break
-
-    # Check if any pages were added to the merger
+ 
     if merger.pages:
         with open(output_file, 'wb') as output_pdf:
             merger.write(output_pdf)
@@ -37,16 +36,13 @@ def find_and_merge_pdfs(directories, roll_number, output_file):
     else:
         print("No pages found meeting the criteria. Merged PDF not created.")
 
-# Prompt the user for the roll number and output PDF file name
 roll_number = input("Enter your roll number: ")
 output_file_name = input("Enter the name for the merged PDF file (without extension): ")
 
-# Define the base directory containing the result folders
 base_directory = r'C:\Users\Hp\Documents\projects\python\Pokhara University Result Organiser\Result'
 merged_pdf = r'C:\Users\Hp\Documents\projects\python\Pokhara University Result Organiser\MergedPDF'
 output_pdf_path = os.path.join(merged_pdf, f"{roll_number} {output_file_name}.pdf")
 
-# Define the directories for the required years and semesters
 directories = {
     'PU_2016..2017Fall': os.path.join(base_directory, '1. PU_2016..2017Fall'),
     'PU_2016..2017Spring': os.path.join(base_directory, '2. PU_2016..2017Spring'),
@@ -63,8 +59,6 @@ directories = {
     'PU_2022..2023Fall': os.path.join(base_directory, '13. PU_2022..2023Fall'),
 }
 
-# Convert dictionary values to a list
 directories_to_process = list(directories.values())
 
-# Call the function to find and merge the PDFs
 find_and_merge_pdfs(directories_to_process, roll_number, output_pdf_path)
